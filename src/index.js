62de6caf4563416f4730c2f676a542209246762f
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2017-06-08 11:31:19 -0500

    add loader

--- src/index.js
+++ src/index.js
@@ -105,18 +105,6 @@ async function updatePage() {
 
 updatePage()
 
-// Page Loader
-document.ontouchmove = (e) => e.preventDefault()
-window.onload = () => {
-  document.getElementById("loader").style.opacity = "0"
-  // wait for animation to end and then remove
-  setTimeout(() => document.getElementById("loader").style.display = "none", 300)
-
-  // enable scrolling
-  document.querySelector("main").classList.remove("noscroll")
-  document.ontouchmove = () => true
-}
-
 // Mobile Menu
 $(document).ready(() => {
   $('.mobile-menu').click(() => {
