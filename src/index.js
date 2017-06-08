622f704b6372c6eb8b4751a73ee3af286877870e
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2017-06-08 11:16:32 -0500

    add loader

--- src/index.js
+++ src/index.js
@@ -109,6 +109,10 @@ updatePage()
 document.ontouchmove = (e) => e.preventDefault()
 window.onload = () => {
   document.getElementById("loader").style.opacity = "0"
+  // wait for animation to end and then remove
+  setTimeout(() => document.getElementById("loader").style.display = "none", 300)
+
+  // enable scrolling
   document.querySelector("main").classList.remove("noscroll")
   document.ontouchmove = () => true
 }
